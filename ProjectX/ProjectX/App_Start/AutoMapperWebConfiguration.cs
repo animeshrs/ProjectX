d9e896c54d9c9e1d4509d2c4775a05303c15c6ff
Animesh Ranjan <animesh_ranjan@outlook.com>
2020-02-21 13:09:00 +0000

    Automapper (Pilot)

---
+++ ProjectX/ProjectX/App_Start/AutoMapperWebConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+
+namespace ProjectX
+{
+    public class AutoMapperWebConfiguration
+    {
+
+        public static void Configure()
+        {
+            var assemblyNames = new[]
+            {
+                "ProjectX"
+            };
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddMaps(assemblyNames);
+            });
+
+            config.CompileMappings();
+
+        }
+    }
+}
